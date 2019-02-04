d666aa9fa81b3cbca53efaf5534fbb1a5063786f
Samuel Svensson <samuel.svensson.95@gmail.com>
2019-02-04 12:16:47 +0100

    Added contact info in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,7 +8,14 @@ const Footer = (props, context) => {
   return (
     <footer>
       <div className="content-grid">
-        <div style={{ color: textPrimary }}>Made by <a href="https://github.com/samouelllsvensson" target="_blank" rel="noopener noreferrer" style={{ color: colorPrimary }}>Samuel</a> <span role="img" aria-label="heart">❤️</span></div>
+        <div style={{ color: textPrimary }}>Made by <a href="https://github.com/samouelllsvensson" target="_blank" rel="noopener noreferrer" style={{ color: colorPrimary }}>Samuel</a> <span role="img" aria-label="heart">❤️</span>
+        <br/>
+        Mobile: +46 725444332
+        <br/>
+        Email: samuel.svensson.95@gmail.com
+
+        </div>
+
       </div>
     </footer>
   );
