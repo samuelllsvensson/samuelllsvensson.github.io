5ccf45e1d60e378855232d5ed0a9ee5564eb30e2
Samuel Svensson <samuel.svensson.95@gmail.com>
2019-08-29 23:16:20 +0200

    Updates

--- status-web/src/constants/netConfig.js
+++
@@ -1,6 +0,0 @@
-const API_IP = process.env.NODE_ENV === 'production'
-  ? '' : 'http://localhost:52318';
-
-const autoUpdateTime = 900000; // seconds
-
-export { API_IP, autoUpdateTime };
--- status-web/src/ducks/index.js
+++
@@ -1,3 +0,0 @@
-export { default as detailReportState } from './detailReports/reducers';
-export { default as siteReportState } from './siteReports/reducers';
-export { default as sitesState } from './sites/reducers';
